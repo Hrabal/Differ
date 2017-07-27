0e41ec37e0de4cbf1039fb8a3982205a420583d2
Federico Cerchiari <federicocerchiari@gmail.com>
2017-07-27 12:29:48 +0200

    iter differ first commit

--- differ/differ.py
+++ differ/differ.py
@@ -1,28 +1,65 @@
 # coding: utf-8
-from itertools import product
 from datetime import datetime
 from difflib import SequenceMatcher
 from collections import OrderedDict
-from types import ComplexType, FloatType, LongType, IntType, UnicodeType
-DirectComparableTypes = (IntType, LongType, FloatType, ComplexType, UnicodeType)
+from itertools import product, chain, izip_longest
+import __builtin__
+builtin_types = [t for t in __builtin__.__dict__.itervalues() if isinstance(t, type)]
 
 from tools import make_hashable, Match
 
+REMOVAL = 'REMOVAL'
+MODIFICATION = 'MODIFICATION'
+ADDITION = 'ADDITION'
 
 class Difference(object):
     def __init__(self, original, new, key=None, **kwargs):
-        self.original = original
-        self.new = new
+        self.original_val = original
+        self.new_val = new
         self.key = key
-        self.added = self.same = self.removed = set()
-        self.modified = dict()
+        self._added = self._same = self._removed = set()
+        self._modified = dict()
+        mods = kwargs.pop('modified', None)
+        if mods:
+            map(lambda k, v: self.add_difference(k, v), mods.items())
         self.__dict__.update(kwargs)
 
     def __str__(self):
-        return str(self.__dict__)
+        return str({k: v for k, v in self.__dict__.iteritems() if v and k not in ('key', )})
+
+    def __repr__(self):
+        return str(self)
 
     def __bool__(self):
-        return any((self.added, self.modified, self.removed))
+        return any((self._added, self._modified, self._removed))
+
+    def add_difference(self, k, diff):
+        self._modified[k] = diff
+
+    def __iter__(self):
+        iter_couples = zip((MODIFICATION, ADDITION, REMOVAL), (self._modified.iteritems(), enumerate(self._added), enumerate(self._removed)))
+        iterators = map(lambda (typ, itrtr): izip_longest(tuple(), itrtr, fillvalue=typ), iter_couples)
+        for typ, iterator in chain(iterators):
+            for key, difference in iterator():
+                print typ, key, difference
+                if difference:
+                    yield typ, (key, difference)
+
+    @property
+    def same(self):
+        return iter(self._same)
+
+    @property
+    def added(self):
+        return iter(self._added)
+
+    @property
+    def removed(self):
+        return iter(self._removed)
+
+    @property
+    def modified(self):
+        return self._modified.iteritems()
 
 
 class Differ(object):
@@ -33,17 +70,18 @@ class Differ(object):
         self.try_cast = try_cast
         self.exclude_cast = exclude_cast or []
 
-    def diff_finder(func):
-        def wrap(self, *args, **kwargs):
-            if args[0].pop('debug', None):
-                pass
-            return func(self, *args, **kwargs)
-        return wrap
-
     def _get_compare_method(self, obj):
+        def is_comparable(obj):
+            try:
+                type(obj)() == type(obj)()
+                return True
+            except (ValueError, TypeError):
+                return False
+
         compare_methods = OrderedDict([
             (lambda x: isinstance(x, dict), self._dict_compare),
             (lambda x: hasattr(x, '__iter__'), self._iter_compare),
+            (lambda x: type(x) in builtin_types and is_comparable(x), self._leaf_compare),
             (lambda x: hasattr(x, '__eq__'), self._leaf_compare),
             (lambda x: isinstance(x, object), self._obj_compare)
         ])
@@ -77,20 +115,16 @@ class Differ(object):
         else:
             return obj
 
-    @diff_finder
     def _obj_compare(self, o1, o2, key=None):
         o1, o1 = map(lambda x: self._prepare_object(x, key=key), (o2.__dict__, o2.__dict__))
         return self._dict_compare(o1, o1, key=key)
 
-    @diff_finder
     def _leaf_compare(self, v1, v2, key=None):
-        return Difference(v1, v2, key=key, **{'added': {v2} if not v1 and v2 else set(),
-                                              'removed': {v1} if v1 and not v2 else set(),
-                                              'modified': {'origin': v1, 'new': v2} if v1 != v2 else {},
-                                              'same': v1 if v1 == v2 else set()
+        return Difference(v1, v2, key=key, **{'_added': {v2} if not v1 and v2 else set(),
+                                              '_removed': {v1} if v1 and not v2 else set(),
+                                              '_same': v1 if v1 == v2 else set()
                                               })
 
-    @diff_finder
     def _iter_compare(self, l1, l2, key=None):
         diff = Difference(l1, l2, key=key)
         l1_filtered, l2_filtered = map(lambda x: self._prepare_object(x, key=key), (l1, l2))
@@ -113,16 +147,15 @@ class Differ(object):
             match = sorted(matches.get(i, []), key=lambda m: m.ratio, reverse=True)[0]
             el2 = l2_filtered[match.twin_index]
             if match.ratio == 1.0:
-                diff.same.add(make_hashable(el1))
+                diff._same.add(make_hashable(el1))
             elif match.ratio == 0.0 and ar:
-                group, element = ((diff.added, str(el2)), (diff.removed, str(el1)))[ar > 0]
+                group, element = ((diff._added, str(el2)), (diff._removed, str(el1)))[ar > 0]
                 group.add(element)
             else:
                 if el1 != el2:
-                    diff.modified[i] = self._get_compare_method(el1)(el1, el2)
+                    diff._modified[i] = self._get_compare_method(el1)(el1, el2)
         return diff
 
-    @diff_finder
     def _dict_compare(self, d1, d2, key=None):
         diff = Difference(d1, d2, key=key)
         d1_filtered, d2_filtered = map(lambda x: self._prepare_object(x, key=key), (d1, d2))
@@ -134,12 +167,11 @@ class Differ(object):
         for k in intersect_keys:
             v1, v2 = d1_filtered[k], d2_filtered[k]
             if v1 != v2:
-                diff.modified[k] = self._get_compare_method(v1)(v1, v2, k)
+                diff._modified[k] = self._get_compare_method(v1)(v1, v2, k)
             else:
-                diff.same.add(k)
+                diff._same.add(k)
         return diff
 
-    @diff_finder
     def compare(self, obj1, obj2):
         method1, method2 = map(self._get_compare_method, (obj1, obj2))
         if method1 != method2:
